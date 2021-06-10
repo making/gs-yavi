54a0dc9dcab8407616d20bfac5ae33528d938b8b
Toshiaki Maki <tmaki@vmware.com>
2021-06-10 14:20:13 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Getting Started Guide for YAVI
+
+https://github.com/making/yavi/blob/develop/docs/GettingStarted.md
---
+++ src/main/java/com/example/Car.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import am.ik.yavi.builder.ValidatorBuilder;
+import am.ik.yavi.core.Validator;
+
+public class Car {
+	private final String manufacturer;
+
+	private final String licensePlate;
+
+	private final int seatCount;
+
+	public static Validator<Car> validator = ValidatorBuilder.<Car>of()
+			.constraint(Car::getManufacturer, "manufacturer", c -> c.notNull())
+			.constraint(Car::getLicensePlate, "licensePlate", c -> c.notNull().greaterThanOrEqual(2).lessThanOrEqual(14))
+			.constraint(Car::getSeatCount, "seatCount", c -> c.greaterThanOrEqual(2))
+			.build();
+
+	public Car(String manufacturer, String licencePlate, int seatCount) {
+		this.manufacturer = manufacturer;
+		this.licensePlate = licencePlate;
+		this.seatCount = seatCount;
+	}
+
+	public String getManufacturer() {
+		return manufacturer;
+	}
+
+	public String getLicensePlate() {
+		return licensePlate;
+	}
+
+	public int getSeatCount() {
+		return seatCount;
+	}
+}
